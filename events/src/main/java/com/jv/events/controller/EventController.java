e05dfba32e28289f667d935f02b097d61088faee
JoaoVitorML-BR <jvmlira33@gmail.com>
2025-08-05 10:36:26 -0300

    refactor: adjusted event creation controller

--- events/src/main/java/com/jv/events/controller/EventController.java
+++ events/src/main/java/com/jv/events/controller/EventController.java
@@ -5,7 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.jv.events.dto.EventCreateDTO;
 import com.jv.events.dto.EventResponseDTO;
+import com.jv.events.models.Event;
+import com.jv.events.service.EventService;
 
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,14 +19,31 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/events")
+@RequiredArgsConstructor
 public class EventController {
+    
+    private final EventService eventService;
+
     @PostMapping
     public ResponseEntity<EventResponseDTO> createEvent(@Valid @RequestBody EventCreateDTO eventCreateDTO) {
-        EventResponseDTO eventResponseDTO = new EventResponseDTO();
-        eventResponseDTO.setId("1");
-        eventResponseDTO.setEventName(eventCreateDTO.getEventName());
-        eventResponseDTO.setDateTime(eventCreateDTO.getDateTime());
-        eventResponseDTO.setCep(eventCreateDTO.getCep());
-        return ResponseEntity.ok(eventResponseDTO);
+        try {
+            Event event = new Event();
+            event.setEventName(eventCreateDTO.getEventName());
+            event.setDateTime(eventCreateDTO.getDateTime());
+            event.setLocation(eventCreateDTO.getCep());
+            event.setCanceled(false);
+            
+            Event savedEvent = eventService.createEvent(event);
+            
+            EventResponseDTO eventResponseDTO = new EventResponseDTO();
+            eventResponseDTO.setId(savedEvent.getId());
+            eventResponseDTO.setEventName(savedEvent.getEventName());
+            eventResponseDTO.setDateTime(savedEvent.getDateTime());
+            eventResponseDTO.setCep(savedEvent.getLocation());
+            
+            return ResponseEntity.status(HttpStatus.CREATED).body(eventResponseDTO);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
