e79d531567a16d2709c4e39c70a91a080ee0c5bf
JoaoVitorML-BR <jvmlira33@gmail.com>
2025-08-07 09:14:48 -0300

    feat: added dto for active tickets

---
+++ events/src/main/java/com/jv/events/dto/TicketCheckResponseDTO.java
@@ -0,0 +1,16 @@
+package com.jv.events.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TicketCheckResponseDTO {
+    private String eventId;
+    private boolean hasTickets;
+    private String message;
+    private long activeTicketCount;
+    private long totalTicketCount;
+}
