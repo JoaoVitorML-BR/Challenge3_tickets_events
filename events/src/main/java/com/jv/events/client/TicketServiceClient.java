3598dd19b6c852229359dc59f5039bd894d7e514
JoaoVitorML-BR <jvmlira33@gmail.com>
2025-08-11 13:16:35 -0300

    refactor: adjusted route to call ticket api

--- events/src/main/java/com/jv/events/client/TicketServiceClient.java
+++ events/src/main/java/com/jv/events/client/TicketServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.jv.events.dto.TicketCheckResponseDTO;
 
-@FeignClient(name = "ticket-service", url = "http://localhost:8081", fallback = TicketServiceFallback.class)
+@FeignClient(name = "ticket-service", url = "ec2-3-143-221-128.us-east-2.compute.amazonaws.com:8081", fallback = TicketServiceFallback.class)
 public interface TicketServiceClient {
 
     @GetMapping("/api/v1/tickets/event/{eventId}/check")
