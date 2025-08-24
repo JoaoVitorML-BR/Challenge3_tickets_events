3870b3451e92f96159811855bf3a09ea5ae83890
JoaoVitorML-BR <jvmlira33@gmail.com>
2025-08-24 19:23:41 -0300

    update url from apis aws

--- ticket/src/main/java/com/jv/ticket/ticket/client/EventServiceClient.java
+++ ticket/src/main/java/com/jv/ticket/ticket/client/EventServiceClient.java
@@ -10,7 +10,7 @@ import com.jv.ticket.ticket.dto.EventPageResponseDTO;
 
 @FeignClient(
     name = "event-service",
-    url = "http://ec2-18-222-140-58.us-east-2.compute.amazonaws.com:8080",
+    url = "http://ec2-13-58-194-161.us-east-2.compute.amazonaws.com:8080",
     fallback = EventServiceFallback.class
 )
 public interface EventServiceClient {
