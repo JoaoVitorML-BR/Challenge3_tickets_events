47ec8c8e179f231e2b36df862258728286facf91
JoaoVitorML-BR <jvmlira33@gmail.com>
2025-11-10 19:58:14 -0300

    fix: ajust router to localhost test

--- events/src/main/java/com/jv/events/client/TicketServiceClient.java
+++ events/src/main/java/com/jv/events/client/TicketServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.jv.events.dto.TicketCheckResponseDTO;
 
-@FeignClient(name = "ticket-service", url = "http://ec2-3-16-161-51.us-east-2.compute.amazonaws.com:8081", fallback = TicketServiceFallback.class)
+@FeignClient(name = "ticket-service", url = "http://localhost:8081", fallback = TicketServiceFallback.class)
 public interface TicketServiceClient {
 
     @GetMapping("/api/v1/tickets/event/{eventId}/check")
